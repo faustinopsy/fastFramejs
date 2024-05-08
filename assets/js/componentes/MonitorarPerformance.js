1eb342e3cf7f0e956c603df761f68bc51a1f9897
faustinopsy <rodrigohipnose@gmail.com>
2024-05-08 14:24:44 -0300

    Create MonitorarPerformance.js

---
+++ assets/js/componentes/MonitorarPerformance.js
@@ -0,0 +1,52 @@
+class MonitorarPerformance {
+    constructor() {
+      this.observer = new PerformanceObserver((list) => {
+        for (const entry of list.getEntries()) {
+          const entryTypeTranslated = this.traduzirEntrada(entry.entryType);
+          console.log(`${entryTypeTranslated} | ${entry.name}: ${entry.duration.toFixed(2)}ms`);
+        }
+      });
+  
+      this.observer.observe({
+        entryTypes: ['paint', 'mark', 'measure', 'navigation', 'resource', 'longtask']
+      });
+    }
+  
+    marcar(marcarNome) {
+      performance.marcar(marcarNome);
+    }
+  
+    mensurar(inicio, fim, nome) {
+      performance.measure(nome, inicio, fim);
+      const medir = performance.getEntriesByNome(nome)[0];
+      console.log(`${nome}: ${medir.duration.toFixed(2)}ms`);
+      this.clearmarcars(inicio, fim);
+      this.clearMeasure(nome);
+    }
+  
+    clearmarcars(inicio, fim) {
+      performance.clearmarcars(inicio);
+      performance.clearmarcars(fim);
+    }
+  
+    clearMeasure(nome) {
+      performance.clearMeasures(nome);
+    }
+  
+    traduzirEntrada(entryType) {
+      const traducao = {
+        'paint': 'Pintura',
+        'mark': 'Marca',
+        'measure': 'Medição',
+        'navigation': 'Navegação',
+        'resource': 'Recurso',
+        'longtask': 'Tarefa Longa'
+      };
+      return traducao[entryType] || entryType;
+    }
+  }
+//   document.addEventListener('DOMContentLoaded', () => {
+//    new MonitorarPerformance();
+  
+//   });
+  
